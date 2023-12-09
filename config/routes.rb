b424b49339468d5d583dfa4a0a1395229232a89e
rask24 <rask0x18@gmail.com>
2023-12-09 16:43:28 +0900

    implement user page + erblint

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
                registrations: 'users/registrations',
              }
 
+  resources :users, param: :name, path: '/', only: %i[show]
+  # resources :users, param: :username, path: '/', only: [:show, :edit, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
