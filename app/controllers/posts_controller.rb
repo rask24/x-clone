183407606b808c22dc5a3ac5a32985f011a8a1a9
rask24 <rask0x18@gmail.com>
2023-12-13 17:06:16 +0900

    add post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
+  def new
+    @post = Post.new user_id: current_user.id
+  end
+
+  def create
+    @post = Post.new post_params
+    @post.user_id = current_user.id
+    @post.save!
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :user_id)
+  end
 end
