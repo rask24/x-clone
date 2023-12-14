6e1a71b1b7a36db91ba82ee846d0106a2e609d0f
rask24 <rask0x18@gmail.com>
2023-12-14 16:43:29 +0900

    update landing page with link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,12 @@ class ApplicationController < ActionController::Base
   private
 
   def redirect_if_not_logged_in
-    redirect_to root_path unless user_signed_in? || request.path == root_path
+    return if user_signed_in? || is_allowed_path
+    redirect_to root_path
+  end
+
+  def is_allowed_path
+    request.path == root_path || request.path == new_user_registration_path ||
+      request.path == new_user_session_path
   end
 end
