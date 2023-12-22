384505a57a80ac42805d27ca705dec03e6c7ac8d
rask24 <rask0x18@gmail.com>
2023-12-22 15:56:11 +0900

    add column reply_to to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,20 +4,28 @@
 #
 # Table name: posts
 #
-#  id         :bigint           not null, primary key
-#  content    :text             not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  user_id    :bigint           not null
+#  id          :bigint           not null, primary key
+#  content     :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  reply_to_id :bigint
+#  user_id     :bigint           not null
 #
 # Indexes
 #
-#  index_posts_on_user_id  (user_id)
+#  index_posts_on_reply_to_id  (reply_to_id)
+#  index_posts_on_user_id      (user_id)
 #
 # Foreign Keys
 #
+#  fk_rails_...  (reply_to_id => posts.id)
 #  fk_rails_...  (user_id => users.id)
 #
 class Post < ApplicationRecord
   belongs_to :user
+
+  with_options class_name: 'Post' do
+    has_many :replies, foreign_key: :reply_to_id, dependent: :nullify, inverse_of: :parent_post
+    belongs_to :parent_post, optional: true
+  end
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,18 +4,21 @@
 #
 # Table name: posts
 #
-#  id         :bigint           not null, primary key
-#  content    :text             not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  user_id    :bigint           not null
+#  id          :bigint           not null, primary key
+#  content     :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  reply_to_id :bigint
+#  user_id     :bigint           not null
 #
 # Indexes
 #
-#  index_posts_on_user_id  (user_id)
+#  index_posts_on_reply_to_id  (reply_to_id)
+#  index_posts_on_user_id      (user_id)
 #
 # Foreign Keys
 #
+#  fk_rails_...  (reply_to_id => posts.id)
 #  fk_rails_...  (user_id => users.id)
 #
 require 'test_helper'
