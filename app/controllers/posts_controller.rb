2930045c41e61dc0e23bbe398643a30f0aa38108
rask24 <rask0x18@gmail.com>
2024-02-22 15:21:32 +0900

    implement reply

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,22 +4,23 @@ class PostsController < ApplicationController
   def show
     @post = Post.find params[:id]
     @replies = @post.replies.includes(:user).order id: :desc
+    @new_reply = Post.new reply_to_id: params[:id], user_id: current_user.id
     @recommended_users = User.recommend
   end
 
   def new
-    @post = Post.new user_id: current_user.id
+    @post = Post.new
   end
 
   def create
     @post = Post.new post_params
-    @post.user_id = current_user.id
+    @post.user = current_user
     @post.save!
   end
 
   private
 
   def post_params
-    params.require(:post).permit :content, :user_id
+    params.require(:post).permit :content, :user_id, :reply_to_id
   end
 end
